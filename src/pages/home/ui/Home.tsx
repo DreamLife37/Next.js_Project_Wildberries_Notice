846edbb9eb999179e8a1371c632e60be080f4b42
Andrey Schavelev <devandreyit@gmail.com>
2023-05-20 18:12:37 +0500

    Added link bot telgram

--- src/pages/home/ui/Home.tsx
+++ src/pages/home/ui/Home.tsx
@@ -5,7 +5,8 @@ import {useState} from "react";
 import {toast, ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import {WatchList} from "@/widgets/watchList/ui/WatchList";
-import { itemService } from '@/shared/api/itemService';
+import {itemService} from '@/shared/api/itemService';
+import Link from "next/link";
 
 
 export default function Home() {
@@ -26,6 +27,7 @@ export default function Home() {
         }
     })
 
+    // @ts-ignore
     return (
         <>
             <ToastContainer
@@ -50,9 +52,21 @@ export default function Home() {
                                     Подпишись на изменение цены
                                 </h1>
                                 <div className={styles.into__subtitle}>
-                                    Telegram бот пришлет уведомление
+                                    <div className={styles.into__subtitleImg}>
+                                        <svg id="sw-js-blob-svg" viewBox="0 0 90 90" xmlns="http://www.w3.org/2000/svg" version="1.1">
+                                            <defs>
+                                                <linearGradient id="sw-gradient" x1="0" x2="1" y1="1" y2="0">
+                                                    <stop id="stop1" stopColor="rgba(248, 117, 55, 1)" offset="0%"/>
+                                                    <stop id="stop2" stopColor="rgba(251, 168, 31, 1)" offset="100%"/>
+                                                </linearGradient>
+                                            </defs>
+                                            <path fill="url(#sw-gradient)" d="M30.9,-25.6C36.3,-17.9,34.3,-5.3,30.3,4.8C26.3,14.9,20.4,22.4,12.6,26.1C4.9,29.8,-4.7,29.6,-13.5,26.1C-22.3,22.6,-30.5,15.7,-34.5,5.7C-38.6,-4.4,-38.6,-17.6,-32.2,-25.5C-25.7,-33.5,-12.9,-36.2,0,-36.2C12.8,-36.1,25.5,-33.4,30.9,-25.6Z" width="100%" height="100%" transform="translate(50 50)" strokeWidth="0"  stroke="url(#sw-gradient)"/>
+                                        </svg>
+                                    </div>
+                                    <Link className={styles.into__subtitleText} href={'https://t.me/WildberriesNoticeBot'}>Telegram бот пришлет уведомление</Link>
                                 </div>
                             </div>
+
                             <Image className={styles.intro__phone}
                                    src="/phone.png"
                                    width={327}
